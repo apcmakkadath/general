5bfe7fa93bda28e3f556ba6d2235f4b70e6cf570
apcmakkadath <43706701+apcmakkadath@users.noreply.github.com>
2020-11-26 14:59:30 +0400

    Update keepass-backup.bat

--- keepass-backup.bat
+++ keepass-backup.bat
@@ -1,3 +1,3 @@
 rem configure in task scheduler to run the script daily
-copy "C:\Users\<username>\Documents\apc.kdbx" "C:\Users\<username>\Documents\keepass-backup\<your-keepass-file>-backup-%date:~-4,4%%date:~-10,2%%date:~-7,2%-%time:~0,2%%time:~3,2%%time:~6,2%.kdbx"
+copy "C:\Users\<username>\Documents\<your-keepass-file>.kdbx" "C:\Users\<username>\Documents\keepass-backup\<your-keepass-file>-backup-%date:~-4,4%%date:~-10,2%%date:~-7,2%-%time:~0,2%%time:~3,2%%time:~6,2%.kdbx"
 msg "%username%" "keypass backed up successfully"
