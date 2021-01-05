b75d01daedc8d8bee85d42c355b49183a359aadc
apcmakkadath <43706701+apcmakkadath@users.noreply.github.com>
2021-01-05 12:15:13 +0400

    Create scrip-analyzer.bat

---
+++ scrip-analyzer.bat
@@ -0,0 +1,6 @@
+set /p scrip=scrip:
+echo %scrip%
+start chrome "https://www.google.com/search?&q=%scrip%+moneycontrol"
+start chrome "https://www.google.com/search?&q=%scrip%+economic times"
+start chrome "https://www.google.com/search?&q=%scrip%+trendlyne"
+start chrome "https://www.google.com/search?&q=%scrip%+tickertape"
