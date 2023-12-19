95843ed5cd74933ef8072835fde48093a78bca23
apcmakkadath <43706701+apcmakkadath@users.noreply.github.com>
2023-12-19 12:20:13 +0400

    Add files via upload

---
+++ secure-password.ps1
@@ -0,0 +1,17 @@
+﻿### Read and encrypt data
+$password = Read-Host -AsSecureString 
+$encrypted = ConvertFrom-SecureString $password
+$encrypted | out-file -FilePath "lrapikey.encrypted" -Force
+
+
+### Read and decrypt encrypted data
+$encrypteddata = Get-Content "lrapikey.encrypted"
+$passwordsecure = ConvertTo-SecureString $encrypteddata
+$plainpassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwordsecure))
+
+
+### Display plain and encrypted data
+Write-Output "################################"
+Write-Output "Encrypted password is $encrypted"
+Write-Output "################################"
+Write-Output "Plain password is $plainpassword"
