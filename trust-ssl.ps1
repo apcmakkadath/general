26c72e39274a2c270730bc0b510a16f3f7461a30
apcmakkadath <43706701+apcmakkadath@users.noreply.github.com>
2026-03-12 13:04:45 +0400

    Create trust-ssl.ps1

---
+++ trust-ssl.ps1
@@ -0,0 +1,14 @@
+add-type -AssemblyName System.Net.Http
+add-type @"
+        using System.Net;
+        using System.Security.Cryptography.X509Certificates;
+        public class TrustAllCertsPolicy : ICertificatePolicy {
+            public bool CheckValidationResult(
+                ServicePoint srvPoint, X509Certificate certificate,
+                WebRequest request, int certificateProblem) {
+                return true;
+            }
+        }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls10 -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
